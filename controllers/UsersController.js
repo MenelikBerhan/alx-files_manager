ce6133b480151b8c3e155cc87081e40f7050f553
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-03-10 04:34:58 +0300

    Add UsersController class that handles `/users`

---
+++ controllers/UsersController.js
@@ -0,0 +1,45 @@
+// App controller for express router
+import crypto from 'crypto';
+import dbClient from '../utils/db';
+
+/**
+ * Handles express server `/users` endpoint.
+ */
+class UsersController {
+  /**
+   * Creates a new user in database.
+   * @param {Request} req
+   * @param {Response} res
+   */
+  static async postNew(req, res) {
+    // check if email is passed in request body
+    if (!req.body.email) {
+      res.status(400).send({ error: 'Missing email' });
+      return;
+    }
+    // check if password is passed in request body
+    if (!req.body.password) {
+      res.status(400).send({ error: 'Missing password' });
+      return;
+    }
+    const { email, password } = req.body;
+    // check if a user with same email already exists
+    const sameUser = await dbClient.db.collection('users').findOne({ email });
+    if (sameUser) {
+      res.status(400).send({ error: 'Already exist' });
+      return;
+    }
+
+    // hash the password in SHA1. (create hash, add data to hash, then digest)
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+
+    // add new user to db
+    const newUser = { email, password: hashedPassword };
+    const result = await dbClient.db.collection('users').insertOne(newUser);
+
+    // respnd with id & email of created user
+    res.status(201).send({ id: result.insertedId, email });
+  }
+}
+
+export default UsersController;
