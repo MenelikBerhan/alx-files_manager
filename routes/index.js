a01289cd11ef235658a4a24a996157214aa67efa
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-03-10 04:35:18 +0300

    Add `/users` endpoint to router

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 // Express server router
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = Router();
 
 // assign route handlers in router
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
 
 export default router;
