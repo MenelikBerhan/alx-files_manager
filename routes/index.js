2560c900429d4ccfaf6551e4b10bbfd54c5ac9dd
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-03-10 07:17:36 +0300

    Add `/connect`, `/disconnect` & `/users/me` endpoints to router

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 // Express server router
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
 
 const router = Router();
@@ -9,5 +10,8 @@ const router = Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
 
 export default router;
