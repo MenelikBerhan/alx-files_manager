44958a5b8e7295235bf0ef80ed688bcd65567e4e
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-03-11 04:54:58 +0300

    Add `/files` endpoint to router & assign handler

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 import UsersController from '../controllers/UsersController';
 
 const router = Router();
@@ -13,5 +14,6 @@ router.post('/users', UsersController.postNew);
 router.get('/users/me', UsersController.getMe);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
+router.post('/files', FilesController.postUpload);
 
 export default router;
