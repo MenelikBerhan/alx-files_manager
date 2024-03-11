ceac31306ae984ae1225d5b71d4ed6f57c0e5ac2
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-03-11 05:42:33 +0300

    Rename file `fs` to `filesClient`. Debug Checker Failure: incase it conflicts with inbuilt.

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -2,7 +2,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
-import fsClient from '../utils/fs';
+import fsClient from '../utils/filesClient';
 import redisClient from '../utils/redis';
 
 /**
