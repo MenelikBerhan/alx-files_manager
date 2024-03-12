58cf911a481e7210452909805f1c2e4ee2be27b2
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-03-12 21:53:20 +0300

    Use toArray and cleanup

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -153,123 +153,32 @@ class FilesController {
     });
   }
 
-  // /**
-  //  * GET /files
-  //  * Retrieves all users file documents for a specific parentId and with pagination.
-  //  * @param {Request} req Request to server
-  //  * @param {Response} res Response from server
-  //  */
-  // static async getIndex(req, res) {
-  //   // get token from X-Token header in request
-  //   const token = req.get('X-Token');
-  //   if (!token) {
-  //     res.status(401).send({ error: 'Unauthorized' });
-  //     return;
-  //   }
-
-  //   const key = `auth_${token}`;
-  //   // retrive user id from redis
-  //   const userId = await redisClient.get(key);
-  //   if (!userId) {
-  //     res.status(401).send({ error: 'Unauthorized' });
-  //     return;
-  //   }
-
-  //   // retrieve document of given id linked to current user
-  //   const parentId = req.query.parentId || '0';
-  //   if (parentId !== '0' && parentId.length !== 24) {
-  // ObjectId() Argument must be a string of 12 bytes
-  //     res.send([]); // send empty list
-  //     return;
-  //   }
-
-  //   // if parentId is given find users document in it. Else return from root (parentId = 0)
-  //   let filter;
-  //   if (parentId === '0') filter = { userId: new ObjectId(userId), parentId };
-  //   else filter = { userId: new ObjectId(userId), parentId: new ObjectId(parentId) };
-
-  //   // get page no from query string. each page contains 20 documents & page no. starts from 0.
-  //   const page = req.params.page || 0;
-  //   // create pipeline and aggregate
-  //   const pipeline = [
-  //     { $match: filter }, // match based on filter
-  //     { $sort: { _id: 1 } }, // sort by id
-  //     { $skip: parseInt(page, 10) * 20 }, // skip to page
-  //     { $limit: 20 },
-  //   ];
-
-  //   // send list of documents
-  //   const aggCursor = dbClient.db.collection('files').aggregate(pipeline);
-  //   const responseFiles = [];
-  //   for await (const document of aggCursor) {
-  //     responseFiles.push({
-  //       id: document._id.toString(),
-  //       userId: document.userId.toString(),
-  //       name: document.name,
-  //       type: document.type,
-  //       isPublic: document.isPublic,
-  //       parentId: document.parentId === '0' ? 0 : document.parentId.toString(),
-  //     });
-  //   }
-  //   res.send(responseFiles);
-  // }
-
-  static async getIndex(req, res) {
-    const token = req.header('X-Token');
-    if (!token) {
-      res.status(401).send({ error: 'Unauthorized' });
-      return;
-    }
-    const key = `auth_${token}`;
-    const userId = await redisClient.get(key);
-    if (!userId) {
-      res.status(401).send({ error: 'Unauthorized' });
-      return;
-    }
-    const { parentId, page = 0 } = req.query;
-    // const files = dbClient.db.collection('files');
-    let filter;
-    if (!parentId) {
-      filter = { userId: new ObjectId(userId) };
-    // query = { userId: user };
-    } else {
-      filter = { parentId: new ObjectId(parentId), userId: new ObjectId(userId) };
-    }
-    const pipeline = [
-      { $match: filter },
-      { $skip: parseInt(page, 10) * 20 },
-      { $limit: 20 },
-    ];
-    const result = await dbClient.db.collection('files').aggregate(pipeline).toArray();
-    const newArr = result.map(({ _id, ...rest }) => ({ id: _id, ...rest }));
-    res.status(200).send(newArr);
-  }
-
   /**
    * GET /files
    * Retrieves all users file documents for a specific parentId and with pagination.
    * @param {Request} req Request to server
    * @param {Response} res Response from server
    */
-  static async getIndex2(req, res) {
+  static async getIndex(req, res) {
     // get token from X-Token header in request
-    const token = req.get('X-Token');
+    const token = req.header('X-Token');
     if (!token) {
       res.status(401).send({ error: 'Unauthorized' });
       return;
     }
 
-    const key = `auth_${token}`;
     // retrive user id from redis
+    const key = `auth_${token}`;
     const userId = await redisClient.get(key);
     if (!userId) {
       res.status(401).send({ error: 'Unauthorized' });
       return;
     }
 
-    // retrieve document of given id linked to current user
-    const parentId = req.query.parentId || '0';
-    // if (parentId !== '0' && parentId.length !== 24) {
+    // get parentId & page query params
+    const { parentId, page = 0 } = req.query;
+
+    // if (parentId' && parentId !== '0 && parentId.length !== 24) {
     //   // ObjectId() Argument must be a string of 12 bytes
     //   res.send([]); // send empty list
     //   return;
@@ -277,21 +186,21 @@ class FilesController {
 
     // if parentId is given find users document in it. Else return from root (parentId = 0)
     let filter;
-    if (parentId === '0') filter = { userId: new ObjectId(userId) };
-    else filter = { userId: new ObjectId(userId), parentId: new ObjectId(parentId) };
+    if (!parentId) filter = { userId: new ObjectId(userId) };
+    else filter = { parentId: new ObjectId(parentId), userId: new ObjectId(userId) };
 
-    // get page no from query string. each page contains 20 documents & page no. starts from 0.
-    const page = req.params.page || 0;
-    // create pipeline and aggregate
+    // create a pipe line for aggreagate with pagination.
+    // each page contains 20 documents & page no. starts from 0.
     const pipeline = [
-      { $match: filter }, // match based on filter
-      // { $sort: { _id: 1 } }, // sort by id
-      { $skip: parseInt(page, 10) * 20 }, // skip to page
+      { $match: filter },
+      { $skip: parseInt(page, 10) * 20 },
       { $limit: 20 },
     ];
+
+    // after getting list of docs from db send list of documents
     const result = await dbClient.db.collection('files').aggregate(pipeline).toArray();
-    const newArr = result.map(({ _id, ...rest }) => ({ id: _id, ...rest }));
-    res.status(200).json(newArr);
+    const newArr = result.map(({ _id, ...attrs }) => ({ id: _id, ...attrs }));
+    res.status(200).send(newArr);
   }
 
   /**
