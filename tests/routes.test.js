1253725c9c57da5309cc471d2893eeca07efc154
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-03-14 04:54:01 +0300

    Add more endpoint tests & clean up before & after hooks

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -3,57 +3,79 @@
 // test for API endpoints
 import { expect } from 'chai';
 import request from 'request';
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 import sha1 from 'sha1';
+import { v4 as uuid4 } from 'uuid';
 import redisClient from '../utils/redis';
 
 const host = process.env.DB_HOST || 'localhost';
 const port = process.env.DB_PORT || 27017;
 const dbName = process.env.DB_DATABASE || 'files_manager';
-const url = `mongodb://${host}:${port}`;
+const mongoUrl = `mongodb://${host}:${port}`;
 
-function asyncRequestPost(url, headers, body) {
-  return new Promise(function (resolve, reject) {
-    request.post({
-      url,
-      headers,
-      body,
-    }, function (error, res, body) {
-      if (!error) {
-        resolve(body);
-      } else {
-        reject(error);
-      }
-    });
-  });
-}
+// used to make asynchronous post
+// function asyncRequestPost(url, headers, body) {
+//   return new Promise(function (resolve, reject) {
+//     request.post({
+//       url,
+//       headers,
+//       body,
+//     }, function (error, res, body) {
+//       if (!error) {
+//         resolve(body);
+//       } else {
+//         reject(error);
+//       }
+//     });
+//   });
+// }
 
 describe('API endpoints', function () {
   let testDbClient;
   let testDb;
   let usersCount;
   let filesCount;
-  const hashedPassword = sha1('menelik123');
+
+  let user; // to be used for the whole test
+  const userTokenForDisconnect = uuid4(); // deleted after /disconnect test
+  const userToken = uuid4(); // persistent through the whole test
+
   before(async function () {
-    testDbClient = new MongoClient(url, { useUnifiedTopology: true });
+    testDbClient = new MongoClient(mongoUrl, { useUnifiedTopology: true });
     await testDbClient.connect();
     testDb = testDbClient.db(dbName);
-    // to avoid error when running test again (used to create new user)
-    await testDb.collection('users')
-      .deleteOne({ email: 'tayitu@adwa.com' });
+
     // insert user in users db to test email duplicate
-    await testDb.collection('users')
-      .insertOne({ email: 'menelik@adwa.com' });
-    // no. of users & files
+    await testDb.collection('users').insertOne({ email: 'menelik@adwa.com' });
+
+    const userId = new ObjectId();
+    const insertObj = {
+      email: 'minilik@adwa.com', password: sha1('menelik123'), _id: userId,
+    };
+
+    // insert user in db (persistent through the whole test)
+    await testDb.collection('users').insertOne(insertObj);
+    user = {
+      id: userId.toString(), email: 'minilik@adwa.com', password: sha1('menelik123'),
+    };
+
+    // insert token in redis
+    await redisClient.set(`auth_${userTokenForDisconnect}`, userId.toString(), 3000);
+    await redisClient.set(`auth_${userToken}`, userId.toString(), 3000);
+
+    // save initial no. of users & files
     usersCount = await testDb.collection('users').countDocuments();
     filesCount = await testDb.collection('files').countDocuments();
   });
 
   after(async function () {
-    await testDb.collection('users')
-      .deleteOne({ email: 'tayitu@adwa.com' });
-    await testDb.collection('users')
-      .deleteOne({ email: 'menelik@adwa.com' });
+    // remove test inserts into db & redis
+    await testDb.collection('users').deleteOne({ email: 'tayitu@adwa.com' });
+    await testDb.collection('users').deleteOne({ email: 'menelik@adwa.com' });
+    await testDb.collection('users').deleteOne({ email: 'minilik@adwa.com' });
+
+    await redisClient.del(`auth_${userTokenForDisconnect}`);
+    await redisClient.del(`auth_${userToken}`);
   });
 
   describe('GET /status', function () {
@@ -152,6 +174,7 @@ describe('API endpoints', function () {
             expect(responseId.length).to.equal(24);
             const user = await testDb.collection('users').findOne({ email: responseEmail });
             expect(user._id.toString()).to.equal(responseId);
+            const hashedPassword = sha1('menelik123');
             expect(user.password).to.equal(hashedPassword);
             done();
           }
@@ -161,29 +184,6 @@ describe('API endpoints', function () {
   });
 
   describe('/connect, /disconnect & /getMe', function () {
-    const base64Auth = Buffer.from('minilik@adwa.com:menelik123').toString('base64');
-    let user; // to be used for sussessful connect
-    const userTokenForDisconnect = 'e30bb704-b6d0-4824-beb3-040f8c38b8af';
-    const userTokenForGetUser = 'e30bb704-b6d0-4824-beb3-040f8c38b8ae';
-
-    before(async function () {
-      await testDb.collection('users').deleteOne({ email: 'minilik@adwa.com' });
-
-      const url = 'http://localhost:5000/users';
-      const headers = { 'content-type': 'application/json' };
-      const body = JSON.stringify({ email: 'minilik@adwa.com', password: 'menelik123' });
-      const resBody = await asyncRequestPost(url, headers, body);
-      user = JSON.parse(resBody);
-      await redisClient.set(`auth_${userTokenForDisconnect}`, user.id, 3000);
-      await redisClient.set(`auth_${userTokenForGetUser}`, user.id, 3000);
-    });
-    after(async function () {
-      await testDb.collection('users')
-        .deleteOne({ email: 'minilik@adwa.com' });
-      await redisClient.del(`auth_${userTokenForDisconnect}`);
-      await redisClient.del(`auth_${userTokenForGetUser}`);
-    });
-
     it('POST /connect: Returns error when user is not found.', function (done) {
       const fakeAuthHeader = Buffer.from('abc:abc').toString('base64');
       request.get(
@@ -202,6 +202,7 @@ describe('API endpoints', function () {
     });
 
     it('POST /connect: Returns token & save user id in redis.', function (done) {
+      const base64Auth = Buffer.from('minilik@adwa.com:menelik123').toString('base64');
       request.get(
         {
           url: 'http://localhost:5000/connect',
@@ -275,13 +276,149 @@ describe('API endpoints', function () {
       request.get(
         {
           url: 'http://localhost:5000/users/me',
-          headers: { 'X-Token': userTokenForGetUser },
+          headers: { 'X-Token': userToken },
         }, async (err, res, body) => {
           if (err) done(err);
           else {
             const expectedData = { email: user.email, id: user.id };
             expect(res.statusCode).to.equal(200);
             expect(JSON.parse(body)).to.eql(expectedData);
+            done();
+          }
+        },
+      );
+    });
+  });
+
+  describe('POST /files', function () {
+    const url = 'http://localhost:5000/files';
+    const headers = { 'content-type': 'application/json', 'X-Token': userToken };
+    const folderId = (new ObjectId()).toString();
+    const fileId = (new ObjectId()).toString();
+
+    before(async function () {
+      // insert a file & folder file in db
+      await testDb.collection('files')
+        .insertMany([{
+          _id: new ObjectId(folderId),
+          userId: user.id,
+          name: 'images',
+          type: 'folder',
+          isPublic: false,
+          parentId: 0,
+        }, {
+          _id: new ObjectId(fileId),
+          userId: user.id,
+          name: 'testfile.txt',
+          type: 'file',
+          isPublic: false,
+          parentId: 0,
+        }]);
+    });
+
+    after(async function () {
+      // remove inserted files in test
+      await testDb.collection('files').deleteOne({ _id: new ObjectId(folderId) });
+      await testDb.collection('files').deleteOne({ _id: new ObjectId(fileId) });
+    });
+
+    it('If the name is missing, return an error', function (done) {
+      request.post({ url, headers, body: JSON.stringify({}) }, async (err, res, body) => {
+        if (err) done(err);
+        else {
+          expect(res.statusCode).to.equal(400);
+          expect(JSON.parse(body).error).to.equal('Missing name');
+          done();
+        }
+      });
+    });
+
+    it('If the type is missing, return an error', function (done) {
+      request.post({ url, headers, body: JSON.stringify({ name: 'test.txt' }) }, async (err, res, body) => {
+        if (err) done(err);
+        else {
+          expect(res.statusCode).to.equal(400);
+          expect(JSON.parse(body).error).to.equal('Missing type');
+          done();
+        }
+      });
+    });
+
+    it('If the type is not in not `file`, `image` or `folder`, return an error', function (done) {
+      request.post(
+        { url, headers, body: JSON.stringify({ name: 'test.txt', type: 'music' }) },
+        async (err, res, body) => {
+          if (err) done(err);
+          else {
+            expect(res.statusCode).to.equal(400);
+            expect(JSON.parse(body).error).to.equal('Missing type');
+            done();
+          }
+        },
+      );
+    });
+
+    it('If the data is missing and type != folder, return an error', function (done) {
+      request.post(
+        { url, headers, body: JSON.stringify({ name: 'test.txt', type: 'file' }) },
+        async (err, res, body) => {
+          if (err) done(err);
+          else {
+            expect(res.statusCode).to.equal(400);
+            expect(JSON.parse(body).error).to.equal('Missing data');
+            done();
+          }
+        },
+      );
+    });
+
+    it('If no file is present in DB for given parentId, return an error', function (done) {
+      request.post(
+        { url, headers, body: JSON.stringify({ name: 'test.txt', type: 'folder', parentId: 'fakeId' }) },
+        async (err, res, body) => {
+          if (err) done(err);
+          else {
+            expect(res.statusCode).to.equal(400);
+            expect(JSON.parse(body).error).to.equal('Parent not found');
+            done();
+          }
+        },
+      );
+    });
+
+    it('If type of file for given parentId is not a folder, return an error', function (done) {
+      request.post(
+        { url, headers, body: JSON.stringify({ name: 'test.txt', type: 'folder', parentId: fileId }) },
+        async (err, res, body) => {
+          if (err) done(err);
+          else {
+            expect(res.statusCode).to.equal(400);
+            expect(JSON.parse(body).error).to.equal('Parent is not a folder');
+            done();
+          }
+        },
+      );
+    });
+
+    it('For folder, add the new file document in the DB and return the new file', function (done) {
+      request.post(
+        { url, headers, body: JSON.stringify({ name: 'music', type: 'folder', parentId: folderId }) },
+        async (err, res, body) => {
+          if (err) done(err);
+          else {
+            const expectedData = {
+              userId: user.id,
+              name: 'music',
+              type: 'folder',
+              isPublic: false,
+              parentId: folderId,
+            };
+            expect(res.statusCode).to.equal(201);
+            const respBody = JSON.parse(body);
+            const respFileId = respBody.id;
+            delete respBody.id;
+            expect(respBody).to.eql(expectedData);
+            expect(respFileId.length).to.equal(24);
             done();
           }
         },
